3e3be31429419a82f41e6efdb84d80f6cb962329
jabu10245 <jabu.10245@gmail.com>
2022-06-09 23:15:27 +0200

    Added continuous mode

--- bothunter/__main__.py
+++ bothunter/__main__.py
@@ -1,5 +1,7 @@
-from sys import stderr, argv
+from sys import exit, stderr
 from argparse import ArgumentParser
+from time import sleep
+from signal import signal, SIGINT
 
 from bothunter.twitch import get_twitch_chatters
 from bothunter.insights import get_twitch_bots
@@ -31,23 +33,51 @@ def find_bots(channel: str):
     
     return found_bots
 
-def main():
-    parser = ArgumentParser(prog="bothunter", description="Finds lurking bots connected to a Twitch channel.")
-    parser.add_argument("channel", type=str, help="The name of the Twitch channel to scan")
-    
-    args = parser.parse_args()
-    channel = args.channel.lower()
+def report_bots(channel: str, beep: bool):
+    print("\nScanning…", file=stderr)
     bots = find_bots(channel)
     count = len(bots)
+    bell = "\a\a" if beep else ""
 
     for bot in sorted(bots):
         print(bot)
+    
     if count == 0:
         print("No bots found :)", file=stderr)
     elif count == 1:
-        print("\n\a\aFound 1 bot.", file=stderr)
+        print(f"\n{bell}Found 1 bot.", file=stderr)
     else:
-        print(f"\n\a\aFound {count} bots.", file=stderr)
+        print(f"\n{bell}Found {count} bots.", file=stderr)
+
+def main():
+    parser = ArgumentParser(prog="bothunter", description="Finds lurking bots connected to a Twitch channel.")
+    parser.add_argument("channel", type=str, help="The name of the Twitch channel to scan")
+    parser.add_argument("-b", action="store_true", help="Beep when bots were found.")
+    parser.add_argument("-c", action="store_true", help="Continuously checking for bots.")
+    parser.add_argument("-i", metavar="<secs>", type=int, default=120, help="Scan interval in seconds. Defaults to 120. Only used in combination with -c.")
+    
+    args = parser.parse_args()
+    channel = args.channel.lower()
+    beep = args.b
+    continuous = args.c
+    seconds = args.i
+    
+    # handle ctrl-c properly
+    def handle_ctrl_c(received, frame):
+        print("\nbothunter stopped.", file=stderr)
+        exit(0)
+        
+    signal(SIGINT, handle_ctrl_c)
+
+    # one-shot scan
+    if not continuous:
+        report_bots(channel, beep)
+        exit(0)
+
+    # continuous scan:
+    while True:
+        report_bots(channel, beep)
+        sleep(seconds)
 
 if __name__ == "__main__":
     main()
